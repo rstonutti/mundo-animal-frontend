1a56de857d8fc0f68af146914a835aa65353932a
Rodrigo <rstonutti@gmail.com>
2021-11-10 18:44:36 -0300

    Pruebas con un modal

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './sass/index.scss';
 import { MundoAnimalApp } from './MundoAnimalApp';
+
+import './sass/index.scss';
 
 ReactDOM.render(
   <MundoAnimalApp />,
